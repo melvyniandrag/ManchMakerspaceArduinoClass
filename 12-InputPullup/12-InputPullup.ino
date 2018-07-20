12c943b5263b3f9da3b9818ec6e5fb7be2904600
melvyniandrag <melvyniandrag@gmail.com>
2018-07-20 01:58:54 -0400

    Added a few more templates for a tutorial I have to fill in

---
+++ 12-InputPullup/12-InputPullup.ino
@@ -0,0 +1,19 @@
+/**
+ * TODO: Something with analong inputs, demonstrating how the analong values are continuous 
+ * whereas the digital inputs are binary.
+ */
+
+const int ButtonPin = 3;
+const int LEDPin = 5;
+const int BaudRate = 9600; // this is the speed at which data is written over the serial cable.
+// The baud rate depends on the device you are writing to, but for writing to the Arduino serial monitor, 9600 is fine.
+
+void setup() {
+//todo
+
+}
+
+void loop() {
+//todo
+
+}
